6dd15cbe0b1d63738aa0650c1693728605a26088
PRASHANT-RP <prashantrpanibhate@gmail.com>
2026-08-27 20:14:13 +0530

    Add query to find next salary in department

--- day9_queries.sql
+++ day9_queries.sql
@@ -6,3 +6,14 @@ SELECT
     salary,
     salary - LAG(salary, 1) OVER (ORDER BY salary) AS salary_diff
 FROM Employees;
+
+
+-- Q2) Next salary in department
+-- Scenario: Using LEAD() window function with PARTITION BY
+
+SELECT 
+    emp_name, 
+    dept_id, 
+    salary,
+    LEAD(salary, 1) OVER (PARTITION BY dept_id ORDER BY salary) AS next_salary
+FROM Employees;
